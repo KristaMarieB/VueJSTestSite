bd2986127545442b9d8ff41f3929de8c70991037
betcherkm <betcherkm@gmail.com>
2019-04-11 23:57:35 -0700

    Attempting next portion of tutorial

--- VueJSTestSite/wwwroot/js/site.js
+++ VueJSTestSite/wwwroot/js/site.js
@@ -3,5 +3,16 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue World!'
+    },
+    el: "#app-2",
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 });
+
+//var app2 = new vue({
+//    el: "#app-2",
+//    data: {
+//        message: 'You loaded this page on ' + new Date().toLocaleString()
+//    }
+//});
