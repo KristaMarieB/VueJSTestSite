984b4b110775f0246d3bc3a112dafe5d29b9b8d5
betcherkm <betcherkm@gmail.com>
2019-04-11 22:55:12 -0700

    Doing the Getting Started portion on the Vue.js website

--- VueJSTestSite/wwwroot/js/site.js
+++ VueJSTestSite/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue World!'
+    }
+});
