95ce7c3f468d9848e37fea23fd4cc7269ae3ade7
Krista <37194474+KristaMarieB@users.noreply.github.com>
2019-04-14 13:46:33 -0700

    First practice from vue site

--- VueJSTestSite/wwwroot/js/site.js
+++ VueJSTestSite/wwwroot/js/site.js
@@ -1,14 +1,10 @@
 ﻿
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue World!'
-    },
-    el: "#app-2",
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-});
+//var app = new Vue({
+//    el: '#app',
+//    data: {
+//        message: 'Hello Vue World!'
+//    }
+//});
 
 //var app2 = new vue({
 //    el: "#app-2",
@@ -16,3 +12,46 @@ var app = new Vue({
 //        message: 'You loaded this page on ' + new Date().toLocaleString()
 //    }
 //});
+
+//var app3 = new Vue({
+//    el: '#app-3',
+//    data: {
+//        seen: true
+//    }
+//})
+
+//var app4 = new Vue({
+//    el: '#app-4',
+//    data: {
+//        todos: [
+//            { text: 'Get Job' },
+//            { text: 'Pay off debt' },
+//            { text: 'Buy a house' }
+//        ]
+//    },
+//    methods: {
+//        addToDoItem: function () {
+//            var item = document.getElementById(newItem).text;
+//            app4.todos.push({ text: item })
+//        }
+//    }
+//})
+
+//var app5 = new Vue({
+//    el: '#app-5',
+//    data: {
+//        message: 'Hello Vue.js!'
+//    },
+//    methods: {
+//        reverseMessage: function () {
+//            this.message = this.message.split('').reverse().join('')
+//        }
+//    }
+//})
+
+//var app6 = new Vue({
+//    el: '#app-6',
+//    data: {
+//        message: 'Hello Vue!'
+//    }
+//})
